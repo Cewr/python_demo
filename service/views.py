d28c8c7911bd5e1f037ef4c9470114e88ff0753f
Cewr <2241610621@qq.com>
2022-01-19 15:47:11 +0800

    feat: websocket版数据交互

--- service/views.py
+++ service/views.py
@@ -3,6 +3,9 @@ from . models import Chess
 from .serializers import Slll
 import json
 
+import asyncio
+import websockets
+
 
 def chess(request):
     method = request.method
@@ -34,3 +37,44 @@ def chess(request):
         'POST': post
     }[method]() or {'message': 'error'}
     return JsonResponse(data=result)
+
+# -----------------------------
+
+# --------------------------------
+
+
+ls = []
+
+
+async def echo(websocket, path):
+    # ------
+    # async for msg in websocket:
+    #     await websocket.send(msg)
+    # ----
+    ls.append(websocket)
+
+    while True:
+        try:
+            message = await websocket.recv()
+            for item in ls:
+                await item.send(message)
+        except Exception as e:
+            print('e>>>>>', e)
+            ls.remove(websocket)
+            break
+# ---------------------------------------------------------
+
+asyncio.get_event_loop().run_until_complete(
+    websockets.serve(echo, '127.0.0.1', 5678)
+)
+asyncio.get_event_loop().run_forever()
+
+# --------------------------------------------------------
+
+
+# async def main():
+#     async with websockets.serve(echo, "127.0.0.1", 5678):
+#         await asyncio.Future()  # run forever
+
+# if __name__ == "service.views":
+#     asyncio.run(main())
